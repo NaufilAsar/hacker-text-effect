14ceda3caa341b75135b1451d3f56383446910bf
Naufil <naufilasar9028@gmail.com>
2023-02-19 12:50:12 +0530

    Complete.

---
+++ index.js
@@ -0,0 +1,25 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+window.onload = () => {
+  document.querySelector("h1").onmouseover = (event) => {
+    let iteration = 0;
+
+    const interval = setInterval(() => {
+      event.target.innerHTML = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index <= iteration) {
+            return event.target.dataset.value[index];
+          }
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= 10) {
+        clearInterval(interval);
+        console.log(iteration);
+      }
+      iteration += 1;
+    }, 30);
+  };
+};
